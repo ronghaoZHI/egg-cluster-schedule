9db76820877fe1ba3bbe9ca7d126298fed6e8bad
ronghaoZHI <895576961@qq.com>
2022-03-28 11:21:05 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const Agenda = require('agenda'); // bull 
+const Agenda = require('agenda'); //  agenda 或者 bull   
 const mongoConnectionString = "mongodb://127.0.0.1/agenda";
 const agenda = new Agenda({ db: { address: mongoConnectionString } })
 
@@ -6,31 +6,24 @@ module.exports = app => {
   console.log('app.schedule.scene', app)
 
   // 初始化 分布式调度任务
+  // 同理，也可将该逻辑 直接放入 agent 进程中
   // 
   agenda.define(
     "job:send-email-report",
     // { priority: "high", concurrency: 10 },
     async (job) => {
       // const { to } = job.attrs.data;
-
+      // 定义需要执行的 job 
+      
       app.messenger.sendToAgent('xx-job', {});
 
-     
-
-      // 假设发邮件
-      // await emailClient.send({
-      //   to,
-      //   from: "example@example.com",
-      //   subject: "Email Report",
-      //   body: "...",
-      // });
     }
   );
 
-  // 配置 job 执行
+  // 配置 job 执行规则
   (async function () {
     await agenda.start();
-    await agenda.every("0 0 8 * * *", ["job:send-email-report"], {
+    await agenda.every("0 0 8 * * *", "job:send-email-report", {
       to: "admin@example.com",
     });
   })();
